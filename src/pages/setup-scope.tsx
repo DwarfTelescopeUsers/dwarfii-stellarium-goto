12c4ec58f8b6e090ab763d785508486c540bf0f9
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-19 08:25:59 -0700

    add components to setup-scope page

--- src/pages/setup-scope.tsx
+++ src/pages/setup-scope.tsx
@@ -1,3 +1,21 @@
+import ConnectCamera from "@/components/ConnectCamera";
+import ConnectStellarium from "@/components/ConnectStellarium";
+
+import SetLocation from "@/components/SetLocation";
+import { useSetupConnection } from "@/hooks/useSetupConnection";
+import StatusBar from "@/components/shared/StatusBar";
+
 export default function SetupScope() {
-  return <div>SetupScope</div>;
+  useSetupConnection();
+
+  return (
+    <div>
+      <StatusBar mode="setup" />
+      <ConnectCamera />
+      <hr />
+      <ConnectStellarium />
+      <hr />
+      <SetLocation />
+    </div>
+  );
 }
