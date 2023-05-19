42913d56debc5fea69518a4ef60b43f4599f4ba0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-19 09:25:28 -0700

    add componets to goto page

--- src/pages/goto.tsx
+++ src/pages/goto.tsx
@@ -1,3 +1,14 @@
+import ManualGoto from "@/components/ManualGoto";
+import StatusBar from "@/components/shared/StatusBar";
+import { useSetupConnection } from "@/hooks/useSetupConnection";
+
 export default function Goto() {
-  return <div>Goto</div>;
+  useSetupConnection();
+
+  return (
+    <div>
+      <StatusBar mode="goto" />
+      <ManualGoto />
+    </div>
+  );
 }
