ead16a7e0581a4ffb0830880f2b09a82e563c12b
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-21 10:48:55 -0700

    refactor success and error handler for getCoordinates

--- src/lib/geolocation.ts
+++ src/lib/geolocation.ts
@@ -4,26 +4,13 @@ const geoOptions = {
   maximumAge: 0,
 };
 
-function successHandler(
-  position: GeolocationPosition, // eslint-disable-line no-undef
-  callback: (coords: GeolocationCoordinates) => void // eslint-disable-line no-unused-vars, no-undef
-): void {
-  const coords = position.coords;
-  callback(coords);
-}
-
-function errorHandler(err: any): void {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
 export function getCoordinates(
-  callback: (coords: GeolocationCoordinates) => void // eslint-disable-line no-unused-vars, no-undef
+  successCallback: (position: GeolocationPosition) => void, // eslint-disable-line no-unused-vars, no-undef
+  errorCallback: (err: any) => void // eslint-disable-line no-unused-vars, no-undef
 ) {
   navigator.geolocation.getCurrentPosition(
-    (position) => {
-      successHandler(position, callback);
-    },
-    errorHandler,
+    successCallback,
+    errorCallback,
     geoOptions
   );
 }
