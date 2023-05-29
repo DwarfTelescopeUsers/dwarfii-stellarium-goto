3f6f7bb83caaffe9ad5711aa012473a1d6d0d858
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-05-28 21:34:57 -0700

    add methods to process stellarium observation lists

--- src/__tests__/stellarium_utils.test.ts
+++ src/__tests__/stellarium_utils.test.ts
@@ -1,4 +1,8 @@
-import { parseStellariumData } from "@/lib/stellarium_utils";
+import {
+  parseStellariumData,
+  processObservationList,
+  formatObsevationObjectName,
+} from "@/lib/stellarium_utils";
 
 describe("parseStellariumData", () => {
   it("parses object name, RA and declination in hh:mm:ss format", () => {
@@ -37,6 +41,360 @@ describe("parseStellariumData", () => {
     };
     let res = parseStellariumData(text);
 
+    expect(res).toEqual(expected);
+  });
+});
+
+describe("processObservationList", () => {
+  it("formats and sorts a list of observation objects", () => {
+    let observations = [
+      {
+        constellation: "CMa",
+        dec: "-20°45'32\"",
+        designation: "M 41",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "4.50",
+        name: "Little Beehive Cluster",
+        nameI18n: "Little Beehive Cluster",
+        objtype: "open star cluster",
+        ra: "6h45m59.8s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Cyg",
+        dec: "+30°42'18\"",
+        designation: "C 34",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "7.00",
+        name: "Filamentary Nebula",
+        nameI18n: "Filamentary Nebula",
+        objtype: "supernova remnant",
+        ra: "20h45m38.7s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Cas",
+        dec: "+61°26'50\"",
+        designation: "IC 1805",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "6.50",
+        name: "Heart Nebula",
+        nameI18n: "Heart Nebula",
+        objtype: "cluster associated with nebulosity",
+        ra: "2h32m39.6s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Ori",
+        dec: "-2°30'04\"",
+        designation: "IC 434",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "7.30",
+        name: "Horsehead Nebula",
+        nameI18n: "Horsehead Nebula",
+        objtype: "HII region",
+        ra: "5h40m46.7s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Sco",
+        dec: "-32°15'10\"",
+        designation: "M 6",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "4.20",
+        name: "Butterfly Cluster",
+        nameI18n: "Butterfly Cluster",
+        objtype: "open star cluster",
+        ra: "17h40m21.4s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Ari",
+        dec: "+11°16'08\"",
+        designation: "Jupiter",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "-2.09",
+        name: "Jupiter",
+        nameI18n: "Jupiter",
+        objtype: "planet",
+        ra: "2h02m08.0s",
+        type: "Planet",
+      },
+      {
+        constellation: "Leo",
+        dec: "+13°05'33\"",
+        designation: "M 65",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "9.30",
+        name: "Leo Triplet",
+        nameI18n: "Leo Triplet",
+        objtype: "galaxy",
+        ra: "11h18m56.2s",
+        type: "Nebula",
+      },
+
+      {
+        constellation: "Cyg",
+        dec: "+31°44'22\"",
+        designation: "C 33",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "7.00",
+        name: "East Veil Nebula",
+        nameI18n: "East Veil Nebula",
+        objtype: "supernova remnant",
+        ra: "20h56m19.6s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Dor",
+        dec: "-69°45'27\"",
+        designation: "PGC 17223",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "0.90",
+        name: "Large Magellanic Cloud",
+        nameI18n: "Large Magellanic Cloud",
+        objtype: "galaxy",
+        ra: "5h23m31.2s",
+        type: "Nebula",
+      },
+      {
+        constellation: "Gem",
+        dec: "+24°21'08\"",
+        designation: "Venus",
+        fov: 0,
+        isVisibleMarker: true,
+        jd: 0,
+        landscapeID: "",
+        location: "",
+        magnitude: "-4.34",
+        name: "Venus",
+        nameI18n: "Venus",
+        objtype: "planet",
+        ra: "7h36m33.8s",
+        type: "Planet",
+      },
+    ];
+    let expected = [
+      {
+        dec: "+11°16'08\"",
+        designation: "Jupiter",
+        displayName: "Jupiter",
+        magnitude: "-2.09",
+        objtype: "planet",
+        ra: "2h02m08.0s",
+        sortName1: "Jupiter",
+        sortName2: -1,
+      },
+      {
+        dec: "+24°21'08\"",
+        designation: "Venus",
+        displayName: "Venus",
+        magnitude: "-4.34",
+        objtype: "planet",
+        ra: "7h36m33.8s",
+        sortName1: "Venus",
+        sortName2: -1,
+      },
+      {
+        dec: "+31°44'22\"",
+        designation: "C 33",
+        displayName: "C 33 - East Veil Nebula",
+        magnitude: "7.00",
+        objtype: "supernova remnant",
+        ra: "20h56m19.6s",
+        sortName1: "C",
+        sortName2: 33,
+      },
+      {
+        dec: "+30°42'18\"",
+        designation: "C 34",
+        displayName: "C 34 - Filamentary Nebula",
+        magnitude: "7.00",
+        objtype: "supernova remnant",
+        ra: "20h45m38.7s",
+        sortName1: "C",
+        sortName2: 34,
+      },
+      {
+        dec: "-2°30'04\"",
+        designation: "IC 434",
+        displayName: "IC 434 - Horsehead Nebula",
+        magnitude: "7.30",
+        objtype: "HII region",
+        ra: "5h40m46.7s",
+        sortName1: "IC",
+        sortName2: 434,
+      },
+      {
+        dec: "+61°26'50\"",
+        designation: "IC 1805",
+        displayName: "IC 1805 - Heart Nebula",
+        magnitude: "6.50",
+        objtype: "cluster associated with nebulosity",
+        ra: "2h32m39.6s",
+        sortName1: "IC",
+        sortName2: 1805,
+      },
+      {
+        dec: "-32°15'10\"",
+        designation: "M 6",
+        displayName: "M 6 - Butterfly Cluster",
+        magnitude: "4.20",
+        objtype: "open star cluster",
+        ra: "17h40m21.4s",
+        sortName1: "M",
+        sortName2: 6,
+      },
+      {
+        dec: "-20°45'32\"",
+        designation: "M 41",
+        displayName: "M 41 - Little Beehive Cluster",
+        magnitude: "4.50",
+        objtype: "open star cluster",
+        ra: "6h45m59.8s",
+        sortName1: "M",
+        sortName2: 41,
+      },
+      {
+        dec: "+13°05'33\"",
+        designation: "M 65",
+        displayName: "M 65 - Leo Triplet",
+        magnitude: "9.30",
+        objtype: "galaxy",
+        ra: "11h18m56.2s",
+        sortName1: "M",
+        sortName2: 65,
+      },
+      {
+        dec: "-69°45'27\"",
+        designation: "PGC 17223",
+        displayName: "PGC 17223 - Large Magellanic Cloud",
+        magnitude: "0.90",
+        objtype: "galaxy",
+        ra: "5h23m31.2s",
+        sortName1: "PGC",
+        sortName2: 17223,
+      },
+    ];
+
+    let res = processObservationList(observations);
+    expect(res).toEqual(expected);
+  });
+});
+
+describe("formatObsevationObjectName", () => {
+  let properties = {
+    constellation: "Leo",
+    dec: "+13°05'33\"",
+    fov: 0,
+    isVisibleMarker: true,
+    jd: 0,
+    landscapeID: "",
+    location: "",
+    magnitude: "9.30",
+    objtype: "galaxy",
+    ra: "11h18m56.2s",
+    type: "Nebula",
+  };
+
+  it("uses designation, name, and nameI18n to create name if they are different", () => {
+    let object = {
+      designation: "M 1",
+      name: "name",
+      nameI18n: "nameI18n",
+      ...properties,
+    };
+    let expected = "M 1 - name; nameI18n";
+
+    let res = formatObsevationObjectName(object);
+    expect(res).toEqual(expected);
+  });
+
+  it("uses designation and name if name and nameI18n are the same", () => {
+    let object = {
+      designation: "M 1",
+      name: "name",
+      nameI18n: "name",
+      ...properties,
+    };
+    let expected = "M 1 - name";
+
+    let res = formatObsevationObjectName(object);
+    expect(res).toEqual(expected);
+  });
+
+  it("uses designation and nameI18n to create name if designation and name are the same", () => {
+    let object = {
+      designation: "M 1",
+      name: "M 1",
+      nameI18n: "nameI18n",
+      ...properties,
+    };
+    let expected = "M 1 - nameI18n";
+
+    let res = formatObsevationObjectName(object);
+    expect(res).toEqual(expected);
+  });
+
+  it("uses designation to create name if designation, name, nameI18n are the same", () => {
+    let object = {
+      designation: "M 1",
+      name: "M 1",
+      nameI18n: "M 1",
+      ...properties,
+    };
+    let expected = "M 1";
+
+    let res = formatObsevationObjectName(object);
+    expect(res).toEqual(expected);
+  });
+
+  it("ignores names that are empty strings", () => {
+    let object = {
+      designation: "M 1",
+      name: "",
+      nameI18n: "",
+      ...properties,
+    };
+    let expected = "M 1";
+
+    let res = formatObsevationObjectName(object);
     expect(res).toEqual(expected);
   });
 });
--- src/lib/stellarium_utils.ts
+++ src/lib/stellarium_utils.ts
@@ -1,7 +1,39 @@
-import { ParsedStellariumData } from "@/types";
+import {
+  ParsedStellariumData,
+  StellariumObjectInfo,
+  StellariumObservationObject,
+  ObservationObject,
+} from "@/types";
 
 export let statusPath = "/api/main/status";
+export let focusPath = "/api/main/focus?target=";
 export let objectInfoPath = "/api/objects/info?format=json";
+
+let catalogs = ["C", "Ced", "HIP", "IC", "LBN", "M", "NGC", "PGC"];
+
+export let typesObjectTypes = {
+  clusters: [
+    "cluster associated with nebulosity",
+    "open star cluster",
+    "globular star cluster",
+    "star cluster",
+    "custom object",
+  ],
+  galaxies: ["galaxy", "active galaxy"],
+  nebulae: [
+    "bipolar nebula",
+    "supernova remnant",
+    "HII region",
+    "planetary nebula",
+    "emission nebula",
+    "interstellar matter",
+    "dark nebula",
+    "reflection nebula",
+    "stellar association",
+  ],
+  stars: ["double star, pulsating variable star", "double star"],
+  solar_system: ["moon", "planet"],
+};
 
 export function parseStellariumData(text: string): ParsedStellariumData {
   let data = {} as ParsedStellariumData;
@@ -33,4 +65,92 @@ function parseObjectName(text: string) {
   if (matches) {
     return { objectName: matches[1].split("<br")[0] };
   }
+}
+
+export function formatObjectName(objectData: StellariumObjectInfo) {
+  let allNames = new Set([
+    objectData.designations,
+    objectData.name,
+    objectData["localized-name"],
+  ]);
+
+  let names = objectData.designations;
+  if (objectData.designations !== objectData.name) {
+    names += `: ${objectData.name}`;
+  }
+  if (objectData.name !== objectData["localized-name"]) {
+    if (allNames.size === 3) {
+      names += `; ${objectData["localized-name"]}`;
+    } else {
+      names += `: ${objectData["localized-name"]}`;
+    }
+  }
+
+  return names;
+}
+
+export function formatObsevationObjectName(
+  objectData: StellariumObservationObject
+) {
+  const { designation, name, nameI18n } = objectData;
+  let allNames = new Set([designation, name, nameI18n]);
+
+  let names = designation;
+  if (designation !== name && name !== "") {
+    names += ` - ${name}`;
+  }
+  if (name !== nameI18n && nameI18n !== "") {
+    if (allNames.size === 3) {
+      names += `; ${nameI18n}`;
+    } else {
+      names += ` - ${nameI18n}`;
+    }
+  }
+
+  return names;
+}
+
+export function isNumeric(value: string) {
+  return /^-?\d+$/.test(value);
+}
+
+function formatObservation(
+  object: StellariumObservationObject
+): ObservationObject {
+  let data = {
+    dec: object.dec,
+    designation: object.designation,
+    magnitude: object.magnitude,
+    objtype: object.objtype,
+    ra: object.ra,
+    displayName: formatObsevationObjectName(object),
+  } as ObservationObject;
+
+  let parts = object.designation.split(" ");
+  if (catalogs.includes(parts[0]) && parts.length === 2) {
+    data.sortName1 = parts[0];
+    data.sortName2 = Number(parts[1]);
+  } else {
+    data.sortName1 = object.designation;
+    data.sortName2 = -1;
+  }
+  return data;
+}
+
+export function processObservationList(
+  objects: StellariumObservationObject[]
+): ObservationObject[] {
+  let formattedObjects = objects.map(formatObservation);
+  let noCatalogObjects = formattedObjects
+    .filter((obj) => obj.sortName2 == -1)
+    .sort((a, b) => a.sortName1.localeCompare(b.sortName1));
+  let catalogObjects = formattedObjects
+    .filter((obj) => obj.sortName2 !== -1)
+    .sort((a, b) => {
+      return (
+        a.sortName1.localeCompare(b.sortName1) || a.sortName2 - b.sortName2
+      );
+    });
+
+  return noCatalogObjects.concat(catalogObjects);
 }
