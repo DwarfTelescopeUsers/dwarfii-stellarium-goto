bde29ca94ecff836b56a30d44e440f602fa9931c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-06-22 02:40:58 -0700

    update home page copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,18 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <h1>Dwarf II + Stellarium</h1>
-
+      <h1>Dwarf II App</h1>
       <p>
-        This website connects to the Dwarf II telescope to Stellarium via the{" "}
-        <Link href="https://hj433clxpv.feishu.cn/docx/MiRidJmKOobM2SxZRVGcPCVknQg">
-          Dwarf II API
-        </Link>{" "}
-        and Stellarium remote control plugin. Once Dwarf II and Stellarium are
-        connected, you can select an object in Stellarium, and then tell Dwarf
-        II to go to that object. You can also select an object from a predefined
-        object lists and view that object in Stellarium.
+        This website allows you to control parts of the Dwarf II using the Dwarf
+        API.
       </p>
-
-      <Link href="/setup-scope" className="btn btn-primary">
-        Start
-      </Link>
+      Features:
+      <ul>
+        <li>Observation list with over 250 objects.</li>
+        <li>Import observation lists from Telescopius.</li>
+        <li>Connect to Stellarium planeterium app to help select targets.</li>
+        <li>Take Astro photos.</li>
+      </ul>
     </div>
   );
 }
