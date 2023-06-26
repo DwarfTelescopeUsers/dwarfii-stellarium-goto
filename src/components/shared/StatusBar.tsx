94a5dba57fce841f51a1da5c5af3fb95d561651d
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-06-25 23:38:27 -0700

    add stellarium connection and astro settings to status bar

--- src/components/shared/StatusBar.tsx
+++ src/components/shared/StatusBar.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ConnectionContext } from "@/stores/ConnectionContext";
-
+import { IRCut } from "dwarfii_api";
 type PropType = {
   mode: string;
 };
@@ -8,21 +8,38 @@ export default function StatusBar(props: PropType) {
   const { mode } = props;
   let connectionCtx = useContext(ConnectionContext);
 
-  let connection = connectionCtx.connectionStatus
-    ? "Connected"
-    : "Not Connected";
+  let connection = connectionCtx.connectionStatus ? (
+    <i className="bi bi-check-circle" style={{ color: "green" }}></i>
+  ) : (
+    <i className="bi bi-x-circle" style={{ color: "red" }}></i>
+  );
 
-  let coords =
-    `Lat: ${connectionCtx.latitude?.toFixed(4) || "--"}, ` +
-    `Lon: ${connectionCtx.longitude?.toFixed(4) || "--"}`;
+  let connectionStellarium = connectionCtx.connectionStatusStellarium ? (
+    <i className="bi bi-check-circle" style={{ color: "green" }}></i>
+  ) : (
+    <i className="bi bi-x-circle" style={{ color: "red" }}></i>
+  );
 
   if (mode) {
     return (
       <div className="mb-2">
         {/* {JSON.stringify(connectionCtx, null, 2)}
         <br /> */}
-        <span>{connection}</span>
-        <span className="ms-4"> {coords}</span>
+        <span className="me-3">Dwarf II: {connection}</span>
+        <span className="me-3">Stellarium: {connectionStellarium}</span>
+        {connectionCtx.astroSettings.gain !== undefined && (
+          <span className="me-3">Gain: {connectionCtx.astroSettings.gain}</span>
+        )}
+        {connectionCtx.astroSettings.exposure !== undefined && (
+          <span className="me-3">
+            Exp: {connectionCtx.astroSettings.exposure}
+          </span>
+        )}
+        {connectionCtx.astroSettings.IR !== undefined && (
+          <span className="me-3">
+            IR: {connectionCtx.astroSettings.IR === IRCut ? "Cut" : "Pass"}
+          </span>
+        )}
       </div>
     );
   } else {
