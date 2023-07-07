adcd54f227a1d24ca3c3ec57a5da1e4437db1d10
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-07-07 07:36:36 -0700

    Revert "add github action to deploy to github pages"
    This reverts commit 6c831c41695a1241e39ceb3a6e3cc366d73e1ae0.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-const isGithubActions = process.env.GITHUB_ACTIONS ? true : false;
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   reactStrictMode: true,
   output: "export",
   distDir: "dist",
