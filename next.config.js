1551291fcedf611501c3ae79edebb09bf461f9c0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-07-07 01:46:34 -0700

    update github action to deploy to github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-/** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS ? true : false;
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   output: "export",
   distDir: "dist",
